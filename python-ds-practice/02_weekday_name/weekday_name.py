c442abab08916d01cf7b15aaf471f3dbe2260b2b
Sauvik Chakraborty <sc@email>
2021-01-27 03:18:23 -0500

    initial commit

---
+++ python-ds-practice/02_weekday_name/weekday_name.py
@@ -0,0 +1,25 @@
+def weekday_name(day_of_week):
+    """Return name of weekday.
+
+        >>> weekday_name(1)
+        'Sunday'
+
+        >>> weekday_name(7)
+        'Saturday'
+
+    For days not between 1 and 7, return None
+
+        >>> weekday_name(9)
+        >>> weekday_name(0)
+    """
+    week = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+
+    }
+    return week.get(day_of_week, None)
